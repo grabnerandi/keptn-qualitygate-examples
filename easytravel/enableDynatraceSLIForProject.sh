0853175d50b817a91a72f1a6ec9c6d81a1930134
grabnerandi <grabnerandi@TAG009442636466.clients.dynatrace.org>
2019-11-20 13:55:34 +0100

    fixes

--- easytravel/enableDynatraceSLIForProject.sh
+++ easytravel/enableDynatraceSLIForProject.sh
@@ -5,8 +5,8 @@ DT_TENANT=$(cat creds_dt.json | jq -r '.dynatraceTenant')
 DT_API_TOKEN=$(cat creds_dt.json | jq -r '.dynatraceApiToken')
 
 ## Create Global Dynatrace SLI Secret
-echo "DT_TENANT: $DT_TENANT" \
-     "DT_API_TOKEN: $DT_API_TOKEN" > dynatrace_secret.yaml
+echo "DT_TENANT: $DT_TENANT
+DT_API_TOKEN: $DT_API_TOKEN" > dynatrace_secret.yaml
 
 kubectl create secret generic dynatrace-credentials-sample -n "keptn" --from-file=dynatrace-credentials=dynatrace_secret.yaml
 
