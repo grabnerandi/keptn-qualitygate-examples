9532a041b96334331d3460eaf203ab52f409f96f
grabnerandi <grabnerandi@TAG009442636466.clients.dynatrace.org>
2019-12-09 08:30:00 +0100

    updates for beta2

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Keptn Quality Gates Tutorials
 
-**Disclaimer:** these tutorials have been tested with [keptn 0.6.beta](https://keptn.sh/docs/0.6.0/)
+**Disclaimer:** these tutorials have been tested with [keptn 0.6.beta2](https://keptn.sh/docs/0.6.0/)
 
 1. [Tutorial for Keptn Quality Gates ONLY on GKE for Services Monitored with Dynatrace](./sample/README.md)
 2. [Tutorial for Keptn Quality Gates on EKS for Services Deployed w Keptn and Monitored w Dynatrace](./simpleservice/README.md)
--- simpleservice/keptn/enableDynatraceSLIForProject.sh
+++ simpleservice/keptn/enableDynatraceSLIForProject.sh
@@ -11,19 +11,32 @@ if [[ -z "$DT_TENANT" || -z "$DT_API_TOKEN" ]]; then
   exit 1
 fi
 
+if [[ -z "$1" ]]; then
+  echo "You have to specify the Project Name!"
+  echo "Usage: enableDynatraceSLIForProject.sh MyKeptnProject"
+  exit 1
+fi
+
 echo "============================================================="
 echo "Enabling Dynatrace SLI Service for your keptn project: $1"
 echo "Dynatrace SLI will pull data from Dynatrace Tenant: $DT_TENANT"
+echo "Creating secret dynatrace-credentials-$1 in keptn namespace"
 echo "============================================================="
 read -rsp $'Press ctrl-c to abort. Press any key to continue...\n' -n1 key
 
+###################################################################################
+## The Lighthouse Service needs a ConfigMap with the name lighthouse-config-PROJECTNAME
+## This ConfigMap references the SLI Provider to be used, in our case Dynatrace
+###################################################################################
 rm -f ./manifests/gen/lighthouse_source_dynatrace.yaml
 cat ./manifests/lighthouse_source_dynatrace.yaml | \
   sed 's~PROJECT~'"$1"'~' > ./manifests/gen/lighthouse_source_dynatrace.yaml
 
 kubectl apply -f ./manifests/gen/lighthouse_source_dynatrace.yaml
 
-## Create Global Dynatrace SLI Secret
+###################################################################################
+## Dynatrace SLI Provider reads Dynatrace Token information from dynatrace-credentials-PROJECTNAME
+###################################################################################
 echo "DT_TENANT: $DT_TENANT
 DT_API_TOKEN: $DT_API_TOKEN" > dynatrace_secret.yaml
 
